56f04a3e8070fe3140a1b1e04931b6ccdb8daa66
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2025-08-14 16:19:52 +0600

    Replace eslint with biome

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,36 +1,35 @@
-import Placeholder from "@/components/placeholder"
-import { Button } from "@/components/ui/button"
-import { initialTickets } from "@/data"
-import Link from "next/link"
+import Link from "next/link";
+import Placeholder from "@/components/placeholder";
+import { Button } from "@/components/ui/button";
+import { initialTickets } from "@/data";
 
 type TicketPageParams = {
-  params: Promise<{
-    ticketId: string
-  }>
-}
-
-
+	params: Promise<{
+		ticketId: string;
+	}>;
+};
 
 export default async function page({ params }: TicketPageParams) {
-  const { ticketId } = await params
-  const ticket = initialTickets.find(t => t.id === ticketId)
-  if (!ticket) {
-    return (
-      <div className="flex flex-1">
-        <Placeholder label="Ticket not found!"
-          button={
-            <Button variant='outline'>
-              <Link href='/' >Go back to home</Link>
-            </Button>
-          }
-        />
-      </div>
-    )
-  }
-  return (
-    <div>
-      <h2 className="text-lg">{ticket.title}</h2>
-      <p className="text-sm">{ticket.content}</p>
-    </div>
-  )
+	const { ticketId } = await params;
+	const ticket = initialTickets.find((t) => t.id === ticketId);
+	if (!ticket) {
+		return (
+			<div className="flex flex-1">
+				<Placeholder
+					label="Ticket not found!"
+					button={
+						<Button variant="outline">
+							<Link href="/">Go back to home</Link>
+						</Button>
+					}
+				/>
+			</div>
+		);
+	}
+	return (
+		<div>
+			<h2 className="text-lg">{ticket.title}</h2>
+			<p className="text-sm">{ticket.content}</p>
+		</div>
+	);
 }
